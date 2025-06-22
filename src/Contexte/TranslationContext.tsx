da69d67412dd92fe2dcd138c3314f5b546551490
Razafindrazokiny Wallin <wallin.razafindrazokiny@gmail.com>
2025-06-22 23:42:06 +0300

    traduction

--- src/Contexte/TranslationContext.tsx
+++ src/Contexte/TranslationContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext } from 'react';
 import axios from 'axios';
 
 // const API_URL = "https://libretranslate.com"; // API publique
